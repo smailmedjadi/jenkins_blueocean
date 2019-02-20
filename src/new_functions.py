adae899e1a3918bec30c5b4addd0c3b83bb783fd
Smail MEDJADI <Smail.MEDJADI_EXT@bpce.fr>
2019-02-20 14:31:37 +0100

    added another sort feature (insertion sort algo)

--- src/new_functions.py
+++ src/new_functions.py
@@ -6,6 +6,14 @@ def bubbleSort(list):
                 list[i] = list[i+1]
                 list[i+1] = temp
 
-list = [54,26,93,17,77,31,44,55,20]
-bubbleSort(list)
-print(list)
+def insertionSort(list):
+   for index in range(1,len(list)):
+
+     currentvalue = list[index]
+     position = index
+
+     while position>0 and list[position-1]>currentvalue:
+         list[position]=list[position-1]
+         position = position-1
+
+     list[position]=currentvalue
